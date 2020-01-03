d4e5331f58bf4a1d109b9ea5eb5683f97c12fc08
Willem van Duijn <willem.vanduijn@kpn.com>
2020-01-03 15:37:23 +0100

    test timeout

--- static/js/particleAnim.js
+++ static/js/particleAnim.js
@@ -1,37 +1,37 @@
-function elementReady(selector) {
-  return new Promise((resolve, reject) => {
-    let el = document.querySelector(selector);
-    if (el) {
-      resolve(el);
-    }
-    new MutationObserver((mutationRecords, observer) => {
-      // Query for elements matching the specified selector
-      Array.from(document.querySelectorAll(selector)).forEach(element => {
-        resolve(element);
-        //Once we have resolved we don't need the observer anymore.
-        observer.disconnect();
-      });
-    }).observe(document.documentElement, {
-      childList: true,
-      subtree: true
-    });
-  });
-}
+// function elementReady(selector) {
+//   return new Promise((resolve, reject) => {
+//     let el = document.querySelector(selector);
+//     if (el) {
+//       resolve(el);
+//     }
+//     new MutationObserver((mutationRecords, observer) => {
+//       // Query for elements matching the specified selector
+//       Array.from(document.querySelectorAll(selector)).forEach(element => {
+//         resolve(element);
+//         //Once we have resolved we don't need the observer anymore.
+//         observer.disconnect();
+//       });
+//     }).observe(document.documentElement, {
+//       childList: true,
+//       subtree: true
+//     });
+//   });
+// }
 
-elementReady('.wd-void-animation').then(element => {
-  console.log(element); /* ---- CORE ---- */
-  setTimeout(() => {
-    createAnim(element);
-  }, 200);
-});
+// elementReady('.wd-void-animation').then(element => {
+//   console.log(element); /* ---- CORE ---- */
+setTimeout(() => {
+  createAnim();
+}, 1500);
+// });
 
-function createAnim(element) {
+function createAnim() {
   //Do whatever you want now the element is there
   const canvas = document.createElement('canvas');
   const context = canvas.getContext('2d');
   let windowWidth = (canvas.width = window.innerWidth);
   let windowHeight = (canvas.height = window.innerHeight);
-  // let element = document.querySelector('.wd-void-animation');
+  let element = document.querySelector('.wd-void-animation');
   /**/ canvas.id = 'canvas';
   /**/ element.append(canvas);
   /* ---- CORE END ---- */
