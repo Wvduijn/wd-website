33a1e64724e8e6fb0455c491ce302c195536f7e9
Willem van Duijn <willemvanduijn.dev@gmail.com>
2020-12-18 16:30:32 +0100

    Updated gridsome.config and added jsconfig

--- gridsome.config.js
+++ gridsome.config.js
@@ -39,8 +39,6 @@ module.exports = {
             use: 'gridsome-plugin-tailwindcss',
             options: {
                 tailwindConfig: './tailwind.config.js',
-                purgeConfig: {},
-                presetEnvConfig: {},
                 shouldPurge: true,
                 shouldImport: true,
                 shouldTimeTravel: true,
